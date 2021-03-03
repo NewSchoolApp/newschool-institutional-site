d7ba9d1a5ba9af0e1a907b2b992d69e255ad118e
Danilo Silva <danilosoaresdasilva@hotmail.com>
2021-03-02 23:10:02 -0300

    created donor grafit component

---
+++ components/atoms/donor-grafit/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Image } from "./style";
+
+export default function DonateGrafit(){
+    return(
+        <Container>
+            <Image src='be-a-donor-grafit.png'></Image>
+        </Container>
+    );
+}
