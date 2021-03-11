41f3a3a93b742643bb0dc402dd7d27504cd27bbc
Rander Gabriel de Oliveira Rodrigues <randergabriel98@gmail.com>
2021-03-11 01:21:21 -0300

    feat: add our partnership

---
+++ components/organisms/our-partnership/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { COLORS } from "../../../styles/colors"
+import { Title } from "../../atoms/title"
+import { Button, Column, Container, FormContainer, TextContainer, TextInput, TextAreaInput, Row, Paragraph } from "./styles"
+
+export const OurPartnership = () => {
+    return <Container>
+        <TextContainer>
+        <Title color={COLORS.gray}>Nossa parceria pode
+        <Title>mudar vidas</Title></Title>
+        <Paragraph>
+        Saiba como sua empresa, lado a lado com a New School, pode quebrar os paradigmas, e revolucionar a educação de centenas de jovens da quebrada. Deixe seus dados que nosso time entrará em contato para conversarmos melhor.
+        </Paragraph>
+        </TextContainer>
+      <FormContainer>
+        <Column>
+          <TextInput placeholder="Nome"></TextInput>
+          <TextInput placeholder="E-mail"></TextInput>
+          <Row>
+            <Column>
+            <TextInput placeholder="Empresa"></TextInput>
+            </Column>
+            <Column>
+            <TextInput placeholder="Telefone"></TextInput>
+            </Column>
+            
+          </Row>
+          <TextAreaInput placeholder="Mensagem">
+          </TextAreaInput>
+        </Column>
+        <Button>Enviar mensagem</Button>
+      </FormContainer>
+    </Container>
+  }
+  
--- pages/faca-parte.js
+++ pages/faca-parte.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
+import { Title } from '../components/atoms/title'
 import { COLORS } from '../styles/colors'
 import styles from '../styles/Home.module.css'
+import { OurPartnership } from "../components/organisms/our-partnership"
 
 export default function JoinUs() {
   return (
     <div className={styles.container}>
       <h1 style={{color: `${COLORS.primary}`}}> FAÇA PARTE </h1>
+      <OurPartnership />
     </div>
   )
 }
