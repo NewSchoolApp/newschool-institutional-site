77ec493960d71fbd2333c52769f763d197c3d8a4
Leonardo <oliveirabalsa2@gmail.com>
2021-03-10 22:14:48 -0300

    feat: implement volunters cards

---
+++ components/organisms/volunteers/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Container, Title, Partners, Image } from "./style";
+
+export default function Volunteers() {
+  return (
+    <Container>
+        <Title>Quem fecha com a gente:</Title>
+        <Partners>
+            <Image src="mural/mural-1.jpg" />
+            <Image src="mural/mural-2.jpg" />
+            <Image src="mural/mural-3.png" />
+            <Image src="mural/mural-4.jpg" />
+            <Image src="mural/mural-5.jpeg" />
+            <Image src="mural/mural-6.jpeg" />
+            <Image src="mural/mural-7.jpeg" />
+            <Image src="mural/mural-8.jpeg" />
+            <Image src="mural/mural-9.jpeg" />
+            <Image src="mural/mural-10.jpeg" />
+            <Image src="mural/mural-11.jpeg" />
+            <Image src="mural/mural-12.jpeg" />
+            <Image src="mural/mural-13.jpeg" />
+            <Image src="mural/mural-14.jpeg" />
+            <Image src="mural/mural-15.jpeg" />
+            <Image src="mural/mural-16.jpeg" />
+            <Image src="mural/mural-17.jpeg" />
+            <Image src="mural/mural-18.jpeg" />
+            <Image src="mural/mural-19.jpeg" />
+            <Image src="mural/mural-20.jpeg" />
+            <Image src="mural/mural-21.jpeg" />
+            <Image src="mural/mural-22.jpeg" />
+            <Image src="mural/mural-23.jpeg" />
+            <Image src="mural/mural-24.jpeg" />
+            <Image src="mural/mural-25.jpeg" />
+            <Image src="mural/mural-26.jpeg" />
+            <Image src="mural/mural-27.jpeg" />
+            <Image src="mural/mural-28.jpeg" />
+        </Partners>
+    </Container>
+  );
+}
---
+++ components/organisms/volunteers/style.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import { COLORS } from "../../../styles/colors";
+
+export const Container = styled.div`
+  width: 90%;
+  margin: 0 5% 0 8%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2%;
+  @media (max-width: 1000px) {
+    flex-wrap: wrap;
+    height: 100%;
+  }
+`;
+
+export const Partners = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 1400px;
+  margin: 2%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  grid-gap: 2%;
+  flex-wrap: wrap;
+`;
+export const Image = styled.img`
+  width: 176px;
+  margin: 1% 0;
+  height: 176px;
+  border-radius: 16px;
+  filter: grayscale(100%);
+  &:hover {
+    filter: none;
+    opacity: 1;
+    transition: 0.2s ease-in;
+  }
+`;
+
+export const Title = styled.h1`
+  font-weight: 800;
+  font-size: 47px;
+  line-height: 64px;
+  text-align: center;
+  color: ${COLORS.primary};
+
+  margin-bottom: 5%;
+`;
