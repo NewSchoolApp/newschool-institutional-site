caa6aec1ee540afac290571ff19cb0842647c578
Leonardo <oliveirabalsa2@gmail.com>
2021-03-19 15:43:15 -0300

    refactor: container size

--- components/atoms/video-loop/style.js
+++ components/atoms/video-loop/style.js
@@ -11,12 +11,6 @@ export const Video = styled.video`
   width: 100%;
   max-width: 1600px;
 
-  @media (min-width: 1600px) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 auto;
-  }
 `;
 
 export const VideoSource = styled.source``;
